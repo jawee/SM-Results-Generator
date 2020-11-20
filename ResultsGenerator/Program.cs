49bb0915ecb02ca02ea9ab5dc951a37fdc675bfc
Andreas Olsson <olsson.andreas@gmail.com>
2020-11-20 08:25:11 +0100

    Creating full html file directly

--- ResultsGenerator/Program.cs
+++ ResultsGenerator/Program.cs
@@ -12,6 +12,43 @@ namespace ResultsGenerator
     {
         static void Main(string[] args)
         {
+            var top = @"<!DOCTYPE html>
+                        <html lang=""se"">
+                          <head>
+                            <title>SM Resultat</title>
+                            <link href=""https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"" rel=""stylesheet"">
+                            <link href=""style.css"" rel=""stylesheet"">
+                            <meta charset=""utf-8""> 
+                          <link rel=""apple-touch-icon"" sizes=""57x57"" href=""/favicon/apple-icon-57x57.png"">
+                          <link rel=""apple-touch-icon"" sizes=""60x60"" href=""/favicon/apple-icon-60x60.png"">
+                          <link rel=""apple-touch-icon"" sizes=""72x72"" href=""/favicon/apple-icon-72x72.png"">
+                          <link rel=""apple-touch-icon"" sizes=""76x76"" href=""/favicon/apple-icon-76x76.png"">
+                          <link rel=""apple-touch-icon"" sizes=""114x114"" href=""/favicon/apple-icon-114x114.png"">
+                          <link rel=""apple-touch-icon"" sizes=""120x120"" href=""/favicon/apple-icon-120x120.png"">
+                          <link rel=""apple-touch-icon"" sizes=""144x144"" href=""/favicon/apple-icon-144x144.png"">
+                          <link rel=""apple-touch-icon"" sizes=""152x152"" href=""/favicon/apple-icon-152x152.png"">
+                          <link rel=""apple-touch-icon"" sizes=""180x180"" href=""/favicon/apple-icon-180x180.png"">
+                          <link rel=""icon"" type=""image/png"" sizes=""192x192""  href=""/favicon/android-icon-192x192.png"">
+                          <link rel=""icon"" type=""image/png"" sizes=""32x32"" href=""/favicon/favicon-32x32.png"">
+                          <link rel=""icon"" type=""image/png"" sizes=""96x96"" href=""/favicon/favicon-96x96.png"">
+                          <link rel=""icon"" type=""image/png"" sizes=""16x16"" href=""/favicon/favicon-16x16.png"">
+                          <meta name=""msapplication-TileColor"" content=""#ffffff"">
+                          <meta name=""msapplication-TileImage"" content=""/favicon/ms-icon-144x144.png"">
+                          <meta name=""theme-color"" content=""#ffffff"">
+                          </head>
+                          <body>
+                            <div class=""container"">
+                              <h1 class=""text-center"">Kvaltider Okayama</h1>
+                              <table class=""table"">
+                                <thead>
+                                  <tr><th>Placering</th><th>Namn</th><th>Tid</th></td>
+                                </thead>
+                                <tbody>";
+
+            var bottom = @"</tbody>
+                        </div>
+                        </body>
+                        </html>";
             var records = new List<ResultsRow>();
             //convert to utf-8 here: https://subtitletools.com/convert-text-files-to-utf8-online
             var file1 = @"C:\Users\olsso\Documents\Projects\ResultsGenerator\Files\Okayama\eventresult_35813272.csv";
@@ -26,13 +63,15 @@ namespace ResultsGenerator
 
             records.Sort(delegate (ResultsRow r1, ResultsRow r2) { return r1.QualifyTime.CompareTo(r2.QualifyTime); });
 
-            using (var file = new StreamWriter(@"C:\Users\olsso\Documents\Projects\ResultsGenerator\Files\Okayama\res.csv"))
+            using (var file = new StreamWriter(@"C:\Users\olsso\Documents\Projects\ResultsGenerator\Files\Okayama\index.html"))
             {
+                file.WriteLine(top);
                 var count = 0;
                 foreach (var line in records)
                 {
                     file.WriteLine("<tr><td>" + ++count + "</td><td>" + line.Name + "</td><td>" + line.QualifyTime.ToString("mm:ss.fff") + "</td></tr>");
                 }
+                file.WriteLine(bottom);
             }
             
         }
